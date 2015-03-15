d6b5d3966e997d9062962db97fa3fe8f177450fb
dcatteeu <davidcatteeuw@gmail.com>
2015-03-15 15:55:34 +0100

    Fix final issues for v0.9 release.
    Help updated.
    
    Added 16x16@2x and 128x128@2x version of appicon.
    
    Window title set to MacRegex instead of Mac Regex.

--- reggy2/AppDelegate.h
+++ reggy2/AppDelegate.h
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.h
-//  reggy2
+//  MacRegex
 //
 //  Created by David Catteeuw on 02/03/15.
 //  Copyright (c) 2015 David R. Catteeuw. All rights reserved.
--- reggy2/main.m
+++ reggy2/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  reggy2
+//  MacRegex
 //
 //  Created by David Catteeuw on 02/03/15.
 //  Copyright (c) 2015 David R. Catteeuw. All rights reserved.
